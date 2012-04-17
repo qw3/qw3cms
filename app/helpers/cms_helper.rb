71a0dc79616b8ebd8158d36c99deea0ab48fa72e
Marcelo <marcelo.theodorojr@gmail.com>
2012-04-17 17:31:46 -0300

    Helper

--- app/helpers/cms_helper.rb
+++ app/helpers/cms_helper.rb
@@ -1,8 +1,8 @@
 # -*- encoding : utf-8 -*-
 module CmsHelper
 
-	def itens_menu_options_for_select(me)
-		
-	end
+	def itens_menu_options_for_select( me )
+		return options_for_select(ItemMenu.where("menu_id = #{me.menu_id} AND id != #{me.id}").order('nome ASC').collect{ |m| [m.descricao, m.id] }, me.item_menu_pai)
+	  end
 
 end
