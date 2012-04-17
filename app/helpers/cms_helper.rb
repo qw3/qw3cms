b311baf7eca4f861bf16abf4467233abdc976bde
Marcelo <marcelo.theodorojr@gmail.com>
2012-04-17 17:40:17 -0300

    item de menu

--- app/helpers/cms_helper.rb
+++ app/helpers/cms_helper.rb
@@ -2,7 +2,9 @@
 module CmsHelper
 
 	def itens_menu_options_for_select( me )
-		return options_for_select(ItemMenu.where("menu_id = #{me.menu_id} AND id != #{me.id}").order('nome ASC').collect{ |m| [m.descricao, m.id] }, me.item_menu_pai)
+		query = "menu_id = #{me.menu_id} AND id != #{me.id}" unless me.new_record?
+		query = "menu_id = #{me.menu_id}" if me.new_record?
+		return options_for_select(ItemMenu.where(query).order('nome ASC').collect{ |m| [m.descricao, m.id] }, me.item_menu_pai)
 	end
 
 end
