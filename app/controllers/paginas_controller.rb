767fc9c9058eb1945e564d40de15259e4565360a
QW3 <contat@qw3.com.br>
2012-07-12 19:26:34 -0300

    .

--- app/controllers/paginas_controller.rb
+++ app/controllers/paginas_controller.rb
@@ -5,10 +5,6 @@ class PaginasController < FrontendController
     @pagina = Pagina.find( params[:id] )
     @pagina.palavras_chave = "" if @pagina.palavras_chave.blank?
     
-    meta :title => "#{Settings["LOJA.titulo"]} | #{@pagina.titulo_pagina}",
-         :description => @pagina.descricao,
-         :keywords => @pagina.palavras_chave.gsub(";", ",") 
-    
     respond_to do |format|
       format.html #show.html.erb
     end
